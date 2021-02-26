bcc6807cc2cb0ba7b4e849e5c88e3bf88e151d1a
Martin Velinov <54155837+martinvelinov@users.noreply.github.com>
2021-02-26 16:37:58 +0100

    Create script.js

---
+++ MemoryGame/script.js
@@ -0,0 +1,68 @@
+const cardArray = [
+        {
+            name: 'dog',
+            img: 'img/dog1.jpg'
+        },
+        {
+            name: 'rabbit',
+            img: 'img/rabbit1.jpg'
+        },
+        {
+            name: 'parrot',
+            img: 'img/parrot1.jpg'
+        },
+        {
+            name: 'fish',
+            img: 'img/fish1.jpg'
+        },
+        {
+            name: 'wolf',
+            img: 'img/wolf1.jpg'
+        },
+        {
+            name: 'cat',
+            img: 'img/cat.jpg'
+        },
+        {
+            name: 'dog',
+            img: 'img/dog1.jpg'
+        },
+        {
+            name: 'rabbit',
+            img: 'img/rabbit1.jpg'
+        },
+        {
+            name: 'parrot',
+            img: 'img/parrot1.jpg'
+        },
+        {
+            name: 'fish',
+            img: 'img/fish1.jpg'
+        },
+        {
+            name: 'wolf',
+            img: 'img/wolf1.jpg'
+        },
+        {
+            name: 'cat',
+            img: 'img/cat.jpg'
+        },
+
+
+    ]
+
+
+
+    function createBoard() {
+     
+    }
+
+   
+    function checkForMatch() {
+        
+    }
+
+    
+    function flipCard() {
+        
+    }
