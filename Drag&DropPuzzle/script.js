9b0c09fdb0a9d99529760c235ecacb6a1d5e6492
Martin Velinov <54155837+martinvelinov@users.noreply.github.com>
2021-03-17 17:06:40 +0100

    Create script.js

---
+++ Drag&DropPuzzle/script.js
@@ -0,0 +1,39 @@
+const filled = document.querySelector('.filled')
+const empties = document.querySelectorAll('.empty')
+
+filled.addEventListener('dragstart', dragStart)
+filled.addEventListener('dragend', dragEnd)
+
+for (const empty of empties) {
+    empty.addEventListener('dragover', dragOver)
+    empty.addEventListener('dragenter', dragEnter)
+    empty.addEventListener('dragleave', dragLeave)
+    empty.addEventListener('drop', dragDrop)
+}
+
+function dragStart() {
+    this.className += ' hold'
+    setTimeout(() => this.className = 'invisible', 0)
+}
+
+function dragEnd() {
+    this.className = 'filled'
+}
+
+function dragOver(e) {
+    e.preventDefault()
+}
+
+function dragEnter(e) {
+    e.preventDefault()
+    this.className += ' hovered'
+}
+
+function dragLeave() {
+    this.className = 'empty'
+}
+
+function dragDrop() {
+    this.className = 'empty'
+    this.append(filled)
+}
