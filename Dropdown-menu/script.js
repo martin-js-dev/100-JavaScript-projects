0875061b85de64166ad3d6267168911748fcb878
Martin Velinov <54155837+martinvelinov@users.noreply.github.com>
2021-04-14 20:09:16 +0200

    Create script.js

---
+++ Dropdown-menu/script.js
@@ -0,0 +1,14 @@
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
+}
