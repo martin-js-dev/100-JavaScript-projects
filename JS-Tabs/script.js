aa29ba24a1b6cefa9eef54a9fa195fe061a8a64b
Martin Velinov <54155837+martinvelinov@users.noreply.github.com>
2021-04-26 20:04:30 +0200

    Create script.js

---
+++ JS-Tabs/script.js
@@ -0,0 +1,16 @@
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContent = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContent.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
+        target.classList.add('active')
+    })
+})
