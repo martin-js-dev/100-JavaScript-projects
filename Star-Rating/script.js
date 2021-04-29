4731cccf7167bc3bb3f04ea23b6c2691a965d0db
Martin Velinov <54155837+martinvelinov@users.noreply.github.com>
2021-04-29 22:01:27 +0200

    Create script.js

---
+++ Star-Rating/script.js
@@ -0,0 +1,20 @@
+// get the elements from the DOM
+const btn = document.querySelector("button");
+const post = document.querySelector(".post");
+const widget = document.querySelector(".star-widget");
+const editBtn = document.querySelector(".edit");
+
+
+btn.onclick = () => {
+    widget.style.display = "none";
+    post.style.display = "block";
+
+    editBtn.onclick = () => {
+        widget.style.display = "block";
+        post.style.display = "none";
+    }
+
+    return false;
+
+
+}
