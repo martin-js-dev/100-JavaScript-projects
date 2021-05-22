0e254ae2753d645e0b486d031e7b3084e11127cd
Martin Velinov <54155837+martinvelinov@users.noreply.github.com>
2021-05-22 15:07:33 +0200

    Create script.js

---
+++ Library-App/script.js
@@ -0,0 +1,123 @@
+const container = document.querySelector('.container');
+
+document.getElementById('form').addEventListener('submit', addBook);
+document.getElementById('resetBooks').addEventListener('click', resetBookCards);
+container.addEventListener("click", changeBook);
+
+let myLibrary = [];
+
+// GET BOOK FROM USER
+function getBookFromInput(book) {
+  let title = document.getElementById('formTitle').value;
+  let author = document.getElementById('formAuthor').value;
+  let pages = document.getElementById('formPages').value;
+  let isRead = document.querySelector('input[name="formRead"]:checked').value;
+  return new Book(title, author, pages, isRead);
+}
+
+// ADD BOOK TO UI
+function addBook(book) {
+  let newBook = getBookFromInput(book);
+  addBookToLibrary(newBook);
+  displayBooks()
+  closeForm();
+  book.preventDefault();
+  document.getElementById("form").reset();
+}
+
+function resetBookCards() {
+
+  document.getElementById("container1").style.display = "none";
+  myLibrary = [];
+}
+
+// BOOK SETTINGS
+function changeBook(e) {
+  if (e.target.classList.contains("removeButton")) {
+    let index = e.target.getAttribute('data-index');
+    let bookCardToRemove = document.querySelector(`[data-index="${index}"]`);
+    container.removeChild(bookCardToRemove);
+    myLibrary.splice(index, 1);
+  }
+  else if (e.target.classList.contains("readStatusBtn")) {
+    let index = e.target.getAttribute('data-index');
+    let bookCardToChange = document.querySelector(`[data-index="${index}"]`);
+    let readStatus = bookCardToChange.querySelector('.isReadStatus');
+    if (readStatus.innerHTML === "Read") {
+      readStatus.innerHTML = "Not Read";
+      myLibrary[index].isRead = "Not Read";
+    } else {
+      readStatus.innerHTML = "Read";
+      myLibrary[index].isRead = "Read";
+
+    }
+
+  }
+}
+
+// BOOK
+class Book {
+  constructor(
+    title = "Unknown",
+    author = "Unknown",
+    pages = "0",
+    isRead = "Not Read",
+  ) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+  }
+}
+
+function addBookToLibrary(newBook) {
+  myLibrary.push(newBook);
+}
+
+function displayBooks() {
+  makeBookCard(myLibrary[myLibrary.length - 1]);
+}
+
+// DISPLAY CARD ON UI
+function makeBookCard(bookToAdd) {
+  let bookCard = document.createElement('div');
+  let author = document.createElement('h4');
+  let title = document.createElement('h2');
+  let pages = document.createElement('p');
+  let isRead = document.createElement('p');
+  let readStatusBtn = document.createElement('BUTTON');
+  let removeButton = document.createElement('BUTTON');
+
+  isRead.className = 'isReadStatus';
+  readStatusBtn.className = 'readStatusBtn';
+  readStatusBtn.innerText = 'Change Read Status';
+  readStatusBtn.setAttribute("data-index", myLibrary.indexOf(bookToAdd));
+  removeButton.className = 'removeButton';
+  removeButton.innerText = 'Remove Book';
+  removeButton.setAttribute("data-index", myLibrary.indexOf(bookToAdd));
+  bookCard.classList.add('bookCard');
+
+  title.textContent = bookToAdd.title;
+  author.textContent = bookToAdd.author;
+  pages.textContent = `${bookToAdd.pages} pages`;
+  isRead.textContent = bookToAdd.isRead;
+
+  bookCard.append(author);
+  bookCard.append(title);
+  bookCard.append(pages);
+  bookCard.append(isRead);
+  bookCard.append(removeButton);
+  bookCard.append(readStatusBtn);
+  bookCard.setAttribute("data-index", myLibrary.indexOf(bookToAdd));
+  container.append(bookCard);
+}
+
+// FORM
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
