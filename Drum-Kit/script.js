a50ebe0845dabee42427db70c5d9662896e79e4e
Martin Velinov <54155837+martinvelinov@users.noreply.github.com>
2021-05-23 14:51:47 +0200

    Create script.js

---
+++ Drum-Kit/script.js
@@ -0,0 +1,24 @@
+// PLAY SOUND
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add('playing');
+}
+
+//REMOVE TRANSITION
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+//EVENTS
+window.addEventListener('keydown', playSound);
+window.addEventListener('mousedown', playSound);
+window.addEventListener('click', playSound);
+
