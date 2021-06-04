bf11dec1d313b735a16ed273b05f34ff87ca65e9
Martin Velinov <54155837+martinvelinov@users.noreply.github.com>
2021-06-04 20:29:45 +0200

    Create script.js

---
+++ Scroll-Indicator/script.js
@@ -0,0 +1,9 @@
+// RUN FUNCTION ON SCROLL
+window.onscroll = function () { userScroll() };
+
+function userScroll() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("progressBar").style.width = scrolled + "%";
+}
